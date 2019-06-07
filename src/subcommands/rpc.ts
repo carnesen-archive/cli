c8c082554a1082f4b1ceae3adf129e4338559151
Chris Arnesen <chris.arnesen@gmail.com>
2019-06-07 10:21:00 -0700

    Add subcommand "rpc raw" for sending arbitrary payload to back end (#43)
    * Add subcommand "rpc raw" for sending arbitrary payload to back end
    
    * Update cloud-api dependency

--- src/subcommands/rpc.ts
+++ src/subcommands/rpc.ts
@@ -1,27 +1,48 @@
-import { createLeaf, createJsonInput, createBranch } from '@alwaysai/alwayscli';
+import {
+  createLeaf,
+  createJsonInput,
+  createBranch,
+  createStringInput,
+} from '@alwaysai/alwayscli';
 import { rpcMethodSpecs } from '@alwaysai/cloud-api';
 import { RpcClient } from '../rpc-client';
+import { SendRpcData } from '../rpc-client/send-rpc-data';
+import { getBearerToken } from '../cognito-auth';
+import { cloudApiUrl } from '../config/cli-config';
 
-const subcommands = Object.entries(rpcMethodSpecs).map(
-  ([methodName, { description }]) => {
-    return createLeaf({
-      name: methodName,
-      description,
-      options: {
-        args: createJsonInput({ description: 'Arguments as JSON array string' }),
-      },
-      async action(_, { args }) {
-        const rpcClient = await RpcClient();
-        const method = (rpcClient as any)[methodName];
-        const result = await method(...(args || []));
-        return result;
-      },
-    });
+const methods = Object.entries(rpcMethodSpecs).map(([methodName, { description }]) => {
+  return createLeaf({
+    name: methodName,
+    description,
+    args: createJsonInput({
+      placeholder: '<args>',
+      description: 'Method arguments array as a JSON string',
+    }),
+    async action(args) {
+      const rpcClient = await RpcClient();
+      const method = (rpcClient as any)[methodName];
+      const result = await method(...(args || []));
+      return result;
+    },
+  });
+});
+
+const raw = createLeaf({
+  name: 'raw',
+  args: createStringInput({
+    placeholder: '<data>',
+    required: true,
+  }),
+  description: 'Send a custom data payload to the RPC endpoint',
+  async action(data) {
+    const bearerToken = await getBearerToken();
+    const sendRpcData = SendRpcData({ bearerToken, cloudApiUrl });
+    return await sendRpcData(data);
   },
-);
+});
 
 export const rpc = createBranch({
   name: 'rpc',
   description: 'Call the alwaysAI cloud API RPC interface',
-  subcommands,
+  subcommands: [...methods, raw],
 });
