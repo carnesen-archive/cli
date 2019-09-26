373fc069d9319d999eb6f0589dd81ebacb024024
Ryan Malone <ryan@alwaysai.co>
2019-09-26 10:39:34 -0700

    wip

--- src/util/audit.ts
+++ src/util/audit.ts
@@ -1,6 +1,9 @@
 import { RandomString } from './get-random-string';
 import { createWriteStream } from 'fs';
 
+const Analytics = require('analytics-node');
+const analytics = new Analytics('62LYiCfyDB5MKGwKEYdsdY1ZTUY6VXVV');
+
 const noop = (_: string, callback?: () => void) => {
   if (callback) {
     callback();
@@ -13,7 +16,16 @@ export async function openAuditLog(path: string) {
   const uniqueId = RandomString().padEnd(11, ' ');
   const stream = createWriteStream(path, { flags: 'a' });
   const writeToStream: typeof audit = (message, callback) => {
-    stream.write(`${new Date().toISOString()} ${uniqueId} ${message}\n`, callback);
+    const formattedMessage = `${new Date().toISOString()} ${uniqueId} ${message}\n`;
+    console.log('formattedMessage : ', formattedMessage);
+    analytics.track({
+      userId: 'test',
+      event: 'CLI Audit Log',
+      properties: {
+        message,
+      },
+    });
+    stream.write(formattedMessage, callback);
   };
   await new Promise((resolve, reject) => {
     stream.on('error', err => {
